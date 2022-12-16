082274212d8c270599ff4a185ce80b5ac655fc20
Taytepilcher <91669192+Taytepilcher@users.noreply.github.com>
2022-12-16 10:54:08 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,6 +1,7 @@
 let i = 0;
 let n;
 let r =0;
+let drawflag = false;
 function setup() {
    const csize = min(windowWidth, windowHeight);// minimum of width and height of the window 
    createCanvas(csize, csize);// canvas will always be square and fit in the smallest value of width and height
@@ -29,9 +30,21 @@ colorPicker = createColorPicker('white');
 colorPicker.position(15, (height/2)-(4*(height/10)));
 colorPicker1 = createColorPicker('black');
 colorPicker1.position(60, (height/2)-(4*(height/10)));
-
+button1 = createButton('Toggle the Fractal Following your Mouse');// initializes button
+button1.Toclass
+button1.size(100, 100);// intialises buttton size
+button1.position(0,(height/2)+100,);// iniitilizes button location
+button1.mousePressed(buttonAction1);//what function the button calls
+button1.addClass('btn')//css class button is added to
 }
-
+function buttonAction1(){
+if (drawflag==false){
+    drawflag= true;
+}
+else if(drawflag==true){
+    drawflag = false;
+}
+}
 
 function buttonAction(){
     saveCanvas( 'myCanvas', 'jpg');// when button is pressed save canvas image as a jpeg
@@ -39,17 +52,24 @@ function buttonAction(){
 
 
 function draw() {
-    scale(width, height);//x and y values go from 0-1 instead of 0-width/height top left corn is (0,0)
-background(colorPicker1.color())
+    scale(width, height);//x and y values go from 0-1 instead of 0-width/height top left corner is (0,0)
+background(colorPicker1.color(),0)
 stroke(colorPicker.color())//lcol[i]);//white stroke
 strokeWeight(0.002)// has to be small so it fits on 0-1 screeen.
 let val =slider.value();// asign the slider value to a variable
 let val2 =slider2.value()
 n = 3+ floor(val);// the amount of points that will be placed around the circle
 const depth = floor(val2);// the amount of fractals created is based on the slider position from 1 -5
-drawFractal(0.5,0.5,0.4,depth);// calls drawFractal function and passes 0.5 for x and y to center shape, 0.4 so the shape doesnt take up half the screen, and 1 for the amount of fractals that are made.
-
-
+if(drawflag== true){
+    const minsize = min(windowWidth, windowHeight);
+let Nmousex = map(mouseX,0,minsize,0,1)
+let Nmousey = map(mouseY,0,minsize,0,1)
+drawFractal(Nmousex,Nmousey,0.1,depth);
+console.log(mouseX)
+    }
+else if (drawflag == false){
+}
+drawFractal(0.5,0.5,0.1,depth);// calls drawFractal function and passes 0.5 for x and y to center shape, 0.4 so the shape doesnt take up half the screen, and 1 for the amount of fractals that are made.
 }
 function polar(angle, radius){// this uses polar co-ordinate to return a cartisian co-ordinate
 return{
